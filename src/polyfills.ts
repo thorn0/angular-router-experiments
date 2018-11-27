7af902cd4e6843618433d471d8a9325bf3b3a925
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2018-11-27 17:41:35 +0200

    fix: stackblitz/core/issues/758

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -47,6 +47,8 @@
 /** IE10 and IE11 requires the following for the Reflect API. */
 // import 'core-js/es6/reflect';
 
+import 'core-js/es7/reflect';
+
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
