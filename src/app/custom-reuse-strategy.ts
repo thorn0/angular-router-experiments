6941f45bae422cd90b43919b4d348c4e8e13b25e
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2018-12-17 17:43:07 +0200

    fix: reused components sometimes didn't get fresh resolved data

--- src/app/custom-reuse-strategy.ts
+++ src/app/custom-reuse-strategy.ts
@@ -105,7 +105,7 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
       return detachedTree;
     }
 
-    return null;
+    return stored ? stored.handle : null;
   }
 
   shouldReuseRoute(
